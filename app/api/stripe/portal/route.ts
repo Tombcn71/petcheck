b4db7c534dcb360048c59a43b67b900a63ac0f9b
Tom <tbvanreijn@gmail.com>
2026-04-20 12:31:28 +0200

    api error

--- app/api/stripe/portal/route.ts
+++ app/api/stripe/portal/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-01-27", // Gebruik de nieuwste versie
+  apiVersion: "2026-03-25.dahlia",
 });
 
 export async function POST() {
