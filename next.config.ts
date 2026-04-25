ef856fff597d64ed12d607ab251f7dc951b50791
Tom <tbvanreijn@gmail.com>
2026-04-25 20:36:45 +0200

    pwa

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
 import type { NextConfig } from "next";
 
-const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-});
-
 const nextConfig: NextConfig = {
-  experimental: {
-    // @ts-ignore - Dit lost de Turbopack error in Vercel op zonder TS fouten
-    turbopack: {},
-  },
+  /* Geen experimentele rommel meer */
 };
 
-module.exports = withPWA(nextConfig);
+export default nextConfig;
