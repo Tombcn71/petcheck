2bcdeb02214ac88f7255d896434b7589137ac5c3
Tom <tbvanreijn@gmail.com>
2026-04-26 10:12:44 +0200

    claude s opninion

---
+++ public/sw.js
@@ -0,0 +1,16 @@
+self.addEventListener("push", function (event) {
+  if (event.data) {
+    const data = event.data.json();
+    const options = {
+      body: data.body,
+      icon: data.icon || "/icons/icon-192x192.png",
+      badge: "/icons/icon-192x192.png",
+    };
+    event.waitUntil(self.registration.showNotification(data.title, options));
+  }
+});
+
+self.addEventListener("notificationclick", function (event) {
+  event.notification.close();
+  event.waitUntil(clients.openWindow("https://doggyscan.nl"));
+});
